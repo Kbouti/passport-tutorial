530d219b0e80fac0e5121aa1e7faaa75878d0b2a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-23 16:30:21 -0700

    Create auth route

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
+var authRouter = require('./routes/auth');
+
 var app = express();
 
 app.locals.pluralize = require('pluralize');
@@ -23,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
