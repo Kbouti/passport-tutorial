cd1a256000a0d75cd3e0d9ea49fa5bfa1b4e5ad6
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-24 19:43:38 -0700

    Configure login

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var session = require('express-session');
+
+var SQLiteStore = require('connect-sqlite3')(session);
+
 var indexRouter = require('./routes/index');
 
 var authRouter = require('./routes/auth');
@@ -23,6 +28,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(passport.authenticate('session'));
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
